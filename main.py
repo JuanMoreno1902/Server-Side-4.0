ac0147154d3112d2990328b3e35294de56c58b75
JuanMoreno1902 <128493014+JuanMoreno1902@users.noreply.github.com>
2023-03-23 07:47:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from overweigthpackage import OverWeigthPackage
 from standardPackage import StandardPackage
 from address import Address
 from deliver import Deliver
-
-"""from bill import Bill"""
+from bill import Bill
 
 
 Individual = Person(1001979606,"Juan","Moreno Santos")
@@ -14,8 +13,7 @@ Weigth = OverWeigthPackage(5.3, "Pista de carreras", 500000, 10.5)
 Standard = StandardPackage("","","","",5.3)
 Addres3 = Address("","","","","","","","","PradoVerde", "Carrera32", "Cartagena", "Ruta95", "130010")
 Delivery = Deliver("22/03/2023", "9:49 Am", "Juan Moreno Santos", "David Arrieta", "recibir", "+573004689813", "Pista de carrera", "")
-
-"""bil4 = Bill(Box,Addres3,Individual,500000 ,"Credito" ,"38 Cuotas")"""
+bil4 = Bill(Box,Addres3,Individual,500000 ,"Credito" ,"38 Cuotas")
 
 print("Individuo")
 print(Individual)
@@ -29,17 +27,4 @@ print("Direccion")
 print(Addres3)
 print("Entrega")
 print(Delivery)
-
-"""print(bil4)"""
-
-
-"""
-if __name__ == '__main__':
-
-    edwin = Person(id_person=73577376, name="Edwin", last_name="Puertas")
-    edwin.name = "Edwin. A"
-    print(edwin)
-
-    juan = Person()
-    print(juan)
-"""
+print(bil4)
